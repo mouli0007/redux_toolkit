1e208bfc3bf4354ae30659e28427933b5047af0d
Mouli VJ <75278840+mouli0007@users.noreply.github.com>
2023-02-25 00:32:18 +0530

    Delete react_blog_project_1 directory

--- react_blog_project_1/src/Store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import postReducer from "./Features/postSlice";
-import userReducer from "./Features/usersSlice";
-export const store = configureStore({
-  reducer: {
-    posts: postReducer,
-    users: userReducer,
-  },
-});
